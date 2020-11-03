5febdea6f355fc7d624d92e6862d017573b9a78c
Ivan Roth <ivan.h.roth@gmail.com>
2020-11-03 10:05:54 -0800

    modify more references to starter_app and satisfy linter

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,10 @@ from flask_cors import CORS
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 
 
-from starter_app.models import db, User
-from starter_app.api.user_routes import user_routes
+from app.models import db, User
+from app.api.user_routes import user_routes
 
-from starter_app.config import Config
+from app.config import Config
 
 app = Flask(__name__)
 
@@ -15,15 +15,15 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 db.init_app(app)
 
-## Application Security
+# Application Security
 CORS(app)
 @app.after_request
 def inject_csrf_token(response):
     response.set_cookie('csrf_token',
-        generate_csrf(),
-        secure=True if os.environ.get('FLASK_ENV') else False,
-        samesite='Strict' if os.environ.get('FLASK_ENV') else None,
-        httponly=True)
+                        generate_csrf(),
+                        secure=True if os.environ.get('FLASK_ENV') else False,
+                        samesite='Strict' if os.environ.get('FLASK_ENV') else None,
+                        httponly=True)
     return response
 
 
