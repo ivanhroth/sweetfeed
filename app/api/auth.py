53605a197b631faae2c9ef5604fc4dc52ef485be
Ivan Roth <ivan.h.roth@gmail.com>
2020-11-05 14:06:03 -0800

    remove a useless import thoughtlessly added from model

---
+++ app/api/auth.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, jsonify, request, redirect
+from flask_jwt_extended import create_access_token
+from app.models import db, User
+
+bp = Blueprint('auth', __name__, url_prefix='')
+
+
+@bp.route('/signup', methods=['POST'])
+def signup():
+    incoming = request.get_json()
+
+    user = User(
+        username=incoming['username'],
+        password=incoming['password']
+    )
+
+    db.session.add(user)
+
+    try:
+        db.session.commit()
+    except IntegrityError:
+        return jsonify(message='User already exists'), 409
+
+    token = create_access_token(identity=user.username)
+    return jsonify(user=user.to_dict(), token=token)
+
+
+@bp.route('/session', methods=['PUT'])
+def login():
+    incoming = request.get_json()
+    user = User.query.filter_by(username=incoming['username']).one()
+    if user and user.check_password(incoming['password']):
+        token = create_access_token(identity=user.username)
+        return jsonify(user=user.to_dict(), token=token)
+    else:
+        return {'msg': 'Incorrect username or password'}, 400
--- database.py
+++ database.py
@@ -6,6 +6,9 @@ load_dotenv()
 
 with app.app_context():
     db.drop_all()
+
+
+
     db.create_all()
 
     db.session.commit()
