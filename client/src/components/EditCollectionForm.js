ee5aa162b470410406ad8d8dcca68498a4f5a031
Ivan Roth <ivan.h.roth@gmail.com>
2020-11-10 13:11:34 -0800

    restructure form (in progress)

--- client/src/components/EditCollectionForm.js
+++ client/src/components/EditCollectionForm.js
@@ -10,12 +10,16 @@ export default function EditCollectionForm(props) {
         async function retrieveCollection() {
             const res = await fetch(`/collections/${props.id}`);
             collection = await res.json();
+            if (collectionName && collection.name !== collectionName) console.log("change the name in the database");
         }
         retrieveCollection();
     });
 
     const [collectionName, setCollectionName] = useState(collection.name)
+    const [collectionFeeds, setCollectionFeeds] = useState([]) //should use the current list of feeds associated with that collection
+    const [currentResults, setCurrentResults] = useState([])
 
+    //https://feedsearch.dev/api/v1/search?url
     return <>
     <form>
         <label for="name">Collection name</label>
@@ -23,9 +27,18 @@ export default function EditCollectionForm(props) {
             setCollectionName(e.target.value);
         }}></input>
 
-        {}
+        {/*collection.feeds.map(feed => "feed")*/};
 
-        <button>Add Feed</button>
+        <input type="text" value={newFeed} onChange={e => {
+
+        }}></input><button>Add Feed</button>
+        <div class="feed-results">
+            {currentResults.map(feed => {
+                <div class="feed-result">
+                    {/* */}
+                </div>
+            }}
+        </div>
     </form>
     </>
 }
