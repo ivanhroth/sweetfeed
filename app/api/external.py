ba068639ad230ade0e8f780669f4cb390ae2b775
Ivan Roth <ivan.h.roth@gmail.com>
2020-11-12 15:46:17 -0800

    satisfy linter

--- app/api/external.py
+++ app/api/external.py
@@ -4,6 +4,7 @@ import requests as req
 
 external_routes = Blueprint('data', __name__)
 
+
 @external_routes.route('/feedsearch/<str:domain>')
 def search_feed(domain):
     r = req.get('https://feedsearch.dev/api/v1/search?url=' + domain)
