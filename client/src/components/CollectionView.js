e766e05bd09ac1bb2e1a99315e400928c98da935
Ivan Roth <ivan.h.roth@gmail.com>
2020-11-12 19:46:27 -0800

    begin building collection view pane

--- client/src/components/CollectionView.js
+++ client/src/components/CollectionView.js
@@ -15,6 +15,14 @@ export default function CollectionView(collectionId) {
 
     const [currentCollection, setCurrentCollection] = useState({ name: "", feeds: [], id: 0 });
 
+    let feedsContent = [];
+
+    const loadFeedsContent = async () => {
+        for (let i=0; i < currentCollection.feeds.length; i++){
+
+        }
+    }
+
     return <>
            </>
 }
