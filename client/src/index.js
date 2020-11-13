2aa73b52a35007ab2e65c3d089e5323b7da4b7f6
Ivan Roth <ivan.h.roth@gmail.com>
2020-11-13 11:56:12 -0800

    main styling

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="overall-container">
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
