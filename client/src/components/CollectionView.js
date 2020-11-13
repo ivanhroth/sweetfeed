df822fa01faf6c8fb75fadafc641b391a5cf652d
Ivan Roth <ivan.h.roth@gmail.com>
2020-11-12 18:40:00 -0800

    start creating collection view pane

---
+++ client/src/components/CollectionView.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+const CollectionView = collectionId => {
+
+    useEffect(() => {
+        async function retrieveCollection() {
+            if (currentCollection.id === 0){
+                const res = await fetch(`/collections/${props.id}`);
+                collection = await res.json();
+                setCurrentCollection(collection)
+            }
+        }
+        retrieveCollection();
+    });
+
+    const [currentCollection, setCurrentCollection] = useState({ name: "", feeds: [], id: 0 });
+
+    return <>
+           </>
+}
